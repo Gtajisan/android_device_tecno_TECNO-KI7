e4c9ac83b35896a522380bb6fcdd5274cad7a7c5
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-05 22:48:22 +0600

    Being up

--- twrp_TECNO-KI7.mk
+++ twrp_TECNO-KI7.mk
@@ -1,22 +1,22 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024-26 The Android Open Source Project hum kick 
+# Copyright (C) 2024-26 SebaUbuntu's TWRP device tree generator
 #
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.0 make by Farhan 
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp tf stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-KI7 device
 $(call inherit-product, device/tecno/TECNO-KI7/device.mk)
 
 PRODUCT_DEVICE := TECNO-KI7
-PRODUCT_NAME := omni_TECNO-KI7
+PRODUCT_NAME := twrp_TECNO-KI7
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KI7
 PRODUCT_MANUFACTURER := tecno
